938ee292f9337aded6cf215aa75094c505a8ecbb
IruneAI <irunelansorena@gmail.com>
2021-02-09 21:33:39 +0100

    Update logs.py

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -5,7 +5,7 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "bankmarketingprediction"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
@@ -13,3 +13,6 @@ logs = service.get_logs()
 
 for line in logs.split('\n'):
     print(line)
+
+#enabling app insights
+service.update(enable_app_insights=True)
