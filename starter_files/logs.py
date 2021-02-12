8ed13d845d25349622e18d44f5555dfe7c6a0162
IruneAI <irunelansorena@gmail.com>
2021-02-12 16:42:46 +0100

    Update logs.py

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -13,9 +13,6 @@ logs = service.get_logs()
 
 for line in logs.split('\n'):
     print(line)
-<<<<<<< HEAD
-=======
 
->>>>>>> 65c84b93ec2f55d2ffed02b483ba70bac0ac6d0b
 #enable app insights
 service.update(enable_app_insights=True)
